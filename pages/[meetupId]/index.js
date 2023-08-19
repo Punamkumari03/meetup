3d6ff8be6ef5741e3884b4e205d2a68aeab51cd8
Punamkumari03 <punamgupta250602@gmail.com>
2023-08-19 20:30:28 +0530

    Prerendering the powers of Next JS

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,16 +1,56 @@
 import { Fragment } from "react";
 import MeetupDetail from "../../components/meetups/Meetupdetail";
 
-
-function MeetupDetails(){
-    return <Fragment>
-    <MeetupDetail
-    image='https://images.pexels.com/photos/3855951/pexels-photo-3855951.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
-    title='A First Meetup'
-    address='Some Street 5 ,Some City'
-    description='The meetup description' />
+function MeetupDetails() {
+  return (
+    <Fragment>
+      <MeetupDetail
+        image="https://images.pexels.com/photos/3855951/pexels-photo-3855951.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+        title="A First Meetup"
+        address="Some Street 5 ,Some City"
+        description="The meetup description"
+      />
     </Fragment>
-      
+  );
+}
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+      {
+        params: {
+          meetupId: "m3",
+        },
+      },
+    ],
+  };
 }
 
+export async function getStaticProps(context) {
+  //fetch data for a single meetup
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://images.pexels.com/photos/3855951/pexels-photo-3855951.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        id: meetupId,
+        title: "A First Meetup",
+        address: "Some Street 5 ,Some City",
+        description: "The meetup description",
+      },
+    },
+  };
+}
 export default MeetupDetails;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import MeetupList from '../components/meetups/MeetupList'
 
 
@@ -19,14 +19,33 @@ const DUMMY_MEETUPS = [
     },
 ]
 
-function HomePage(){
-    const [loadedMeetups,setLoadedMeetups] = useState([])
-    useEffect(()=>{
-        //send http request and fetch data
-        setLoadedMeetups(DUMMY_MEETUPS);
-    },[]);
-    return <MeetupList meetups={loadedMeetups}/>
+function HomePage(props){
+   
+  
+    return <MeetupList meetups={props.meetups}/>
+
 
 }
+// export async function getServerSideProps(context){
+//     const req = context.req;
+//     const res = context.res;
+//     //fetch data from api
+//     return {
+//         props:{
+//             meetups: DUMMY_MEETUPS
+//     }
 
+//     }
+// }
+
+
+export async function getStaticProps(){
+    //fetch data from api
+    return {
+        props: {
+            meetups: DUMMY_MEETUPS
+        },
+        revalidate: 10
+    }
+}
 export default HomePage;
